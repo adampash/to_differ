dc117b9db284a6a16364946914cd5f0a5ec0dfd9
Adam Pash <adam.pash@gmail.com>
2013-09-30 17:20:54 -0400

    tweaking diff string method. fixes bug, commented, adjusted to fix edge cases

--- app/assets/javascripts/diffstring.js
+++ app/assets/javascripts/diffstring.js
@@ -12,18 +12,21 @@
 function escape(s) {
     var n = s;
     n = n.replace(/&/g, "&amp;");
-    // n = n.replace(/</g, "&lt;");
-    // n = n.replace(/>/g, "&gt;");
+    n = n.replace(/</g, "&ltt;");
+    n = n.replace(/>/g, "&gtt;");
     n = n.replace(/"/g, "&quot;");
 
     return n;
 }
 
 function diffString( o, n ) {
-  o = o.replace(/\s+$/, '');
-  n = n.replace(/\s+$/, '');
   o = o.replace(/(<([^>]+)>)/ig, ' $1 ');
   n = n.replace(/(<([^>]+)>)/ig, ' $1 ');
+  o = o.replace(/\s+$/, '');
+  n = n.replace(/\s+$/, '');
+  console.log(o);
+  console.log(n);
+
 
   var out = diff(o == "" ? [] : o.split(/\s+/), n == "" ? [] : n.split(/\s+/) );
   var str = "";
@@ -44,33 +47,32 @@ function diffString( o, n ) {
   if (out.n.length == 0) {
       for (var i = 0; i < out.o.length; i++) {
 
-        str += '<del>' + escape(out.o[i]) + "</del>" + oSpace[i];
+        str += '$del$' + escape(out.o[i]) + "$/del$" + oSpace[i];
       }
   } else {
-    if (out.n[0].text == null) {
-      for (n = 0; n < out.o.length && out.o[n].text == null; n++) {
-
-        str += '<del>' + escape(out.o[n]) + "</del>" + oSpace[n];
-      }
+    for (n = 0; n < out.o.length && out.o[n].text == null; n++) {
+      str += '$del$' + escape(out.o[n]) + oSpace[n] + "$/del$";
     }
 
     for ( var i = 0; i < out.n.length; i++ ) {
       if (out.n[i].text == null) {
 
-        str += '<ins>' + escape(out.n[i]) + "</ins>" + nSpace[i];
+        str += '$ins$' + escape(out.n[i]) + "$/ins$" + nSpace[i];
       } else {
         var pre = "";
 
         for (n = out.n[i].row + 1; n < out.o.length && out.o[n].text == null; n++ ) {
 
-          pre += '<del>' + escape(out.o[n]) + "</del>" + oSpace[n];
+          pre += '$del$' + escape(out.o[n]) + "$/del$" + oSpace[n];
         }
         str += " " + out.n[i].text + nSpace[i] + pre;
       }
     }
   }
-  str = str.replace(/__linebreak__/g, '<br />')
+
   // str = merge_adjacent(str, ['del', 'ins'])
+  str = str.replace(/&ltt;/g, "wtf").replace(/&gtt;/g, "wtfrighte");
+  console.log(str);
   return str;
 }
 
@@ -79,8 +81,8 @@ function is_html(str) {
 }
 
 function randomColor() {
-    return "rgb(" + (Math.random() * 100) + "%, " + 
-                    (Math.random() * 100) + "%, " + 
+    return "rgb(" + (Math.random() * 100) + "%, " +
+                    (Math.random() * 100) + "%, " +
                     (Math.random() * 100) + "%)";
 }
 function diffString2( o, n ) {
@@ -108,7 +110,7 @@ function diffString2( o, n ) {
       colors[i] = randomColor();
 
       if (out.o[i].text != null) {
-          os += '<span style="background-color: ' +colors[i]+ '">' + 
+          os += '<span style="background-color: ' +colors[i]+ '">' +
                 escape(out.o[i].text) + oSpace[i] + "</span>";
       } else {
           os += "<del>" + escape(out.o[i]) + oSpace[i] + "</del>";
@@ -118,7 +120,7 @@ function diffString2( o, n ) {
   var ns = "";
   for (var i = 0; i < out.n.length; i++) {
       if (out.n[i].text != null) {
-          ns += '<span style="background-color: ' +colors[out.n[i].row]+ '">' + 
+          ns += '<span style="background-color: ' +colors[out.n[i].row]+ '">' +
                 escape(out.n[i].text) + nSpace[i] + "</span>";
       } else {
           ns += "<ins>" + escape(out.n[i]) + nSpace[i] + "</ins>";
@@ -128,43 +130,55 @@ function diffString2( o, n ) {
   return { o : os , n : ns };
 }
 
+function buildWordIndex(words) {
+  var wordsIndex = new Object();
+  for ( var i = 0; i < words.length; i++ ) {
+    if ( wordsIndex[ words[i] ] == null )
+      wordsIndex[ words[i] ] = { rows: new Array(), o: null };
+    wordsIndex[ words[i] ].rows.push( i );
+  }
+  return wordsIndex;
+}
+
 function diff( o, n ) {
-  var ns = new Object();
-  var os = new Object();
-  
-  for ( var i = 0; i < n.length; i++ ) {
-    if ( ns[ n[i] ] == null )
-      ns[ n[i] ] = { rows: new Array(), o: null };
-    ns[ n[i] ].rows.push( i );
-  }
-  
-  for ( var i = 0; i < o.length; i++ ) {
-    if ( os[ o[i] ] == null )
-      os[ o[i] ] = { rows: new Array(), n: null };
-    os[ o[i] ].rows.push( i );
-  }
-  
+  var ns = buildWordIndex(n);
+  var os = buildWordIndex(o);
+
+  // for each word in the new text
   for ( var i in ns ) {
+    // why do we need this undefined check?
     if ( ns[i].rows.length == 1 && typeof(os[i]) != "undefined" && os[i].rows.length == 1 ) {
       n[ ns[i].rows[0] ] = { text: n[ ns[i].rows[0] ], row: os[i].rows[0] };
       o[ os[i].rows[0] ] = { text: o[ os[i].rows[0] ], row: ns[i].rows[0] };
     }
   }
-  
+  // when a word matches at the some location in both strings, make a new object with it that has the index of it in the other string + the text
+
   for ( var i = 0; i < n.length - 1; i++ ) {
-    if ( n[i].text != null && n[i+1].text == null && n[i].row + 1 < o.length && o[ n[i].row + 1 ].text == null && 
-         n[i+1] == o[ n[i].row + 1 ] ) {
+    if (
+      // if this word is not in this position in the original string
+      n[i].text != null
+      // and the next word is unmatched
+      && n[i+1].text == null
+      // and we haven't passed the end of the original string
+      && n[i].row + 1 < o.length
+      // and at the next word in the original string is not matched
+      && o[ n[i].row + 1 ].text == null
+      // and the next word is the same in the origial string
+      && n[i+1] == o[ n[i].row + 1 ]
+      ) {
       n[i+1] = { text: n[i+1], row: n[i].row + 1 };
       o[n[i].row+1] = { text: o[n[i].row+1], row: i + 1 };
     }
   }
-  
+
   for ( var i = n.length - 1; i > 0; i-- ) {
-    if ( n[i].text != null && n[i-1].text == null && n[i].row > 0 && o[ n[i].row - 1 ].text == null && 
+    if ( n[i].text != null && n[i-1].text == null && n[i].row > 0 && o[ n[i].row - 1 ].text == null &&
          n[i-1] == o[ n[i].row - 1 ] ) {
       n[i-1] = { text: n[i-1], row: n[i].row - 1 };
       o[n[i].row-1] = { text: o[n[i].row-1], row: i - 1 };
     }
   }
+
   return { o: o, n: n };
 }
